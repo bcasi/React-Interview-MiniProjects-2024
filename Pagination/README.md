c2a7d5180abb75c01a0893479dc821c5cc5c967c
arnetkc <arnetkc@gmail.com>
2024-08-13 08:25:17 +0530

    pagination-added

---
+++ Pagination/README.md
@@ -0,0 +1,19 @@
+<h1>Interview Topics- Pagination -2024</h1>
+
+<div>
+<img src="/src/assets/pagination.png" alt="pagination">
+</div>
+
+<div>
+  <h2>Sources Used</h2>
+  <ul>
+    <li>
+      <a href="https://adamlynch.com/flexible-data-tables-with-css-grid/"
+        target="_blank" rel="noreferrer noopener" >html and css template for table</a
+      >
+    </li>
+    <li>
+     <a href="https://rajrajhans.com/2021/04/pagination-and-infinite-scroll-in-react/" target="_blank" rel="noopener noreferrer">Pagination for React</a>
+    </li>
+  </ul>
+</div>
---
+++ Pagination/src/App.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Table from "./components/Table";
+import Button from "./components/Button";
+
+const itemsPerPage = 10;
+
+const App = () => {
+  const [members, setMembers] = useState([]);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    async function getMember() {
+      try {
+        const res = await fetch(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+
+        const data = await res.json();
+        setMembers(data);
+      } catch (e) {
+        alert("failed to fetch data");
+        setError("Failed to fetch data");
+      }
+    }
+    getMember();
+  }, []);
+
+  function handlePrevious() {
+    const startIndex = page * itemsPerPage - itemsPerPage;
+    if (startIndex <= 1) return;
+    setPage((page) => page - 1);
+  }
+
+  function handleNext() {
+    const nextEndIndex = page * itemsPerPage;
+    if (nextEndIndex >= members.length) return;
+    setPage((page) => page + 1);
+  }
+
+  //this will be the sliced index data
+  function paginationData() {
+    const startIndex = page * itemsPerPage - itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return members.slice(startIndex, endIndex);
+  }
+
+  return (
+    <div className="main-container">
+      <h1>Employee Data Table</h1>
+      {error ? (
+        <div>{error}</div>
+      ) : (
+        <>
+          <div className="table-container">
+            <Table members={paginationData()} />
+          </div>
+          <div className="button-container">
+            <Button handlePage={handlePrevious}>Previous</Button>
+            <Button isDisabled={true} isNum={true}>
+              {page}
+            </Button>
+            <Button handlePage={handleNext}>Next</Button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
